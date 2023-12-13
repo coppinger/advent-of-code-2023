4aa29bd001b8f57f2dd9b6bec35a07998f5e2940
coppinger <charles.coppinger@gmail.com>
2023-12-13 20:58:39 +1300

    day 2 an%

---
+++ src/routes/2/+page.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  import { Button } from "$lib/components/ui/button";
+
+  let cookieCount = 0;
+
+  function eatCookie() {
+    cookieCount++;
+  }
+
+  function unEatCookie() {
+    cookieCount--;
+  }
+
+  function purgeStomach() {
+    cookieCount = 0;
+  }
+</script>
+
+{cookieCount}
+
+<Button
+  on:click={() => {
+    unEatCookie();
+  }}>Uneat</Button
+>
+<Button
+  on:click={() => {
+    eatCookie();
+  }}>Eat</Button
+>
+<Button
+  on:click={() => {
+    purgeStomach();
+  }}>Purge</Button
+>
